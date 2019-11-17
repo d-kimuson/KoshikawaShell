2acce64559012c59e1dc6b49fd83bada91588f8c
Kaito <mail.kaito03@gmail.com>
2019-11-18 08:56:56 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 script_path=`pwd`"/koshikawa.sh"
+echo "export KOSHIKAWA_ROOT=$(pwd)" >> ~/.bash_profile
 
 chmod a+x ./uninstall.sh
 chmod a+x $script_path
 sed -i -e "s#$script_path##g" ~/.bash_profile
 \rm ~/.bash_profile-e
-echo "$script_path" >> ~/.bash_profile
+printf "Set koshikawa shell as the default shell ? (y/n) >> "; read ans
+if [ "${ans}" != 'n' ]; then
+  echo "$script_path" >> ~/.bash_profile
+fi
