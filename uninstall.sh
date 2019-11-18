c14981c4dca1ecd27aaeffab8d7cde4befbed8ba
Kaito <mail.kaito03@gmail.com>
2019-11-18 09:17:40 +0900

    update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -2,5 +2,14 @@
 
 script_path=`pwd`"/koshikawa.sh"
 
-sed -i -e "s#$script_path##g" ~/.bash_profile
+printf "Remove Koshikawa Shell from default ? (y/n) >> "; read ans
+if [ "${ans}" != 'n' ]; then
+  sed -i -e "s#$script_path##g" ~/.bash_profile
+fi
+
+printf "Uninstall Packeage management tool: wakashiko ? (y/n) >> "; read ans
+if [ "${ans}" != 'n' ]; then
+  cat ~/.bash_profile | grep "source `echo $KOSHIKAWA_ROOT`" | xargs -Itarget sed -i -e "s#target##g" ~/.bash_profile
+fi
+
 \rm ~/.bash_profile-e
